7f086e8b9853f0e21cff2812c748bb5e0de52aa0
Sébastien Labbé <slabqc@gmail.com>
2021-03-04 12:25:19 +0100

    ajout doc init; removed other argument

--- matrix.py
+++ matrix.py
@@ -1,6 +1,18 @@
 
 class Matrice:
     def __init__(self, ncolumns, nrows, L):
+        r"""
+        INPUT:
+
+        - ``ncolumns`` -- entier, nombre de colonnes
+        - ``nrows`` -- entier, nombre de lignes
+        - ``L`` -- liste, les ``ncolumns*nrows`` entrées de la matrice 
+
+        EXAMPLES::
+
+            >>> Matrice(3, 3, [1,2,3,4,5,6,7,8,9])
+
+        """
         self._L = L
         self._ncols = ncols
         self._nrows = nrows
@@ -14,9 +26,9 @@ class Matrice:
     def __mul__(self, other):
         raise NotImplementedError
 
-    def nombre_de_colonnes(self, other):
+    def nombre_de_colonnes(self):
         raise NotImplementedError
 
-    def nombre_de_lignes(self, other):
+    def nombre_de_lignes(self):
         raise NotImplementedError
 
